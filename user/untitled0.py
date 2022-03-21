f81fb855250f86133ef8a09d13b17db15206d98b
emreoztoklu <emreoztoklu@hotmail.com>
2022-03-21 07:50:44 +0300

    Initial commit

--- user/untitled0.py
+++ user/untitled0.py
@@ -3,17 +3,25 @@ import sounddevice as sd
 import matplotlib.pyplot as plt
 import soundfile as sf
 
-fs = 44100
+fs = 128
 sn = 2
 time = np.arange(0,sn,1/fs)
 rate = 20.0
 
-def sound_lenght(second=0):
+amp=[0,1,0.1,0.01,0.001,0.0001]
+freq=[2544,5087,7632,10176,12719,15264]
+
+
+sound1=[]
+sound2=[]
+sound3=[]
+sound4=[]
+
+def sound_lenght(second):
     x=int(44100*second)
     return x
 
-def DFT(x): # To calculate DFT of a 1D real-valued signal x
-    
+def DFT(x): # To calculate DFT of a 1D real-valued signal x   
     N = len(x)
     n = np.arange(N)
     k = n.reshape((N, 1))
@@ -21,11 +29,8 @@ def DFT(x): # To calculate DFT of a 1D real-valued signal x
     X = np.dot(e, x)
     return X
 
-amp=[0,1,0.1,0.01,0.001,0.0001]
-freq=[2544,5087,7632,10176,12719,15264]
 
-sound1=[]
-for i in range (0,sound_lenght(0.4)):
+for i in range (0,sound_lenght(3)):
     sound1.append((amp[2]   *np.sin( 2*np.pi*(freq[0])*  i/fs))
                  +(amp[4]   *np.sin( 2*np.pi*(freq[1])*  i/fs))
                  +(amp[3]   *np.sin( 2*np.pi*(freq[2])*  i/fs))
@@ -33,17 +38,16 @@ for i in range (0,sound_lenght(0.4)):
                  +(amp[3]   *np.sin( 2*np.pi*(freq[4])*  i/fs))
                  +(amp[4]   *np.sin( 2*np.pi*(freq[5])*  i/fs)))
      
-sound2=[]
 for i in range (0,sound_lenght(0.2)):     
     sound2.append(0)
 
-sound3=[]
 for i in range (0,sound_lenght(1)):
     sound3.append((amp[1]   *np.sin( 2*np.pi*(50*i/(1*fs))*  i/fs)))
     
-sound4=[]
 for i in range (0,sound_lenght(1)):
     sound4.append((amp[1]  *np.sin( 2*np.pi*(1000)*  i/fs)))
+   
+    
    
 sound=np.concatenate((sound2,sound1,sound2))    
 sound5=np.concatenate((sound3,sound4))
@@ -69,13 +73,17 @@ sd.play(sound1, 44100)
 sf.write("reverse2.wav",sound,fs)
 sf.write("reverse3.wav",sound5,fs)
 
-X = DFT(sound1[0:4096])
 
+
+
+X = DFT(sound1[0:4096])
 # calculate the frequency
 N = len(X)
 n = np.arange(N)
 T = N/fs
 freq = n/T 
+
+
 
 n_oneside = N//2
 # get the one side frequency
@@ -86,14 +94,12 @@ X_oneside =X[:n_oneside]/n_oneside
 
 plt.figure(figsize = (12, 6))
 plt.subplot(121)
-plt.stem(f_oneside, abs(X_oneside), 'b', \
-         markerfmt=" ", basefmt="-b")
+plt.stem(f_oneside, abs(X_oneside), 'b', markerfmt=" ", basefmt="-b")
 plt.xlabel('Freq (Hz)')
 plt.ylabel('DFT Amplitude |X(freq)|')
 
 plt.subplot(122)
-plt.stem(f_oneside, abs(X_oneside), 'b', \
-         markerfmt=" ", basefmt="-b")
+plt.stem(f_oneside, abs(X_oneside), 'b', markerfmt=" ", basefmt="-b")
 plt.xlabel('Freq (Hz)')
 plt.xlim(0, 5000)
 plt.tight_layout()
